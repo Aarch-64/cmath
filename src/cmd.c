3035a7a1bdbf9d19023f24b402cd9c5721f2aacf
AgustinGutierrez0 <gutierrezaverruz0@hotmail.com>
2022-12-07 21:46:02 -0600

    v3

--- src/cmd.c
+++ src/cmd.c
@@ -31,11 +31,11 @@ void cmd(int argc, char *argv[])
     char hel[] = "--help";
 
     char namever[] = "cmath 0.3\n";
-    char builver[] = "Este programa fue construido para x86_64-pc-linux-gnu\n";
+    char builver[] = "This program was built for x86_64-pc-linux-gnu\n";
     char copyver[] = "Copyright (C) 2022-2022 Agustin Gutierrez.\n";
-    char licever[] = "Licencia GPLv3+: GNU GPL versión 3 o posterior <http://gnu.org/licenses/gpl.html>\n\n";
-    char typever[] = "Este es software libre: cualquiera es libre para redistribuirlo y modificarlo.\n";
-    char garaver[] = "No existe GARANTÍA ALGUNA, hasta los límites permitidos por las leyes aplicables..\n";
+    char licever[] = "GPLv3+ License: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\n";
+    char typever[] = "This is free software: anyone is free to redistribute and modify it.\n";
+    char garaver[] = "There is no WARRANTY, up to the limits permitted by applicable laws.\n";
     
     if (argv[1] == NULL)
     {
