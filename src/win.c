1f773db61c26ab27742abd17ea0311016bccb31d
AgustinGutierrez0 <gutierrezaverruz0@hotmail.com>
2022-12-13 04:52:56 -0600

    first commit

--- src/win.c
+++ src/win.c
@@ -1,22 +1,10 @@
-/* win.c -- distribution and version numbers. */
-
-/* Copyright (C) 2022-2022 Agustin Gutierrez.
-
-   This file is part of cmath, math test in c.
-
-   cmath is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   cmath is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with cmath.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// 
+// win.c
+// cmath
+//
+// Last modification by Aarch-64 on 13/12/2022
+// Copyright © 2022 Aarch-64. All rights reserved.
+//
 
 #include <ncurses.h> // se incluye la libreria ncurses.h
 #include <stdlib.h> // se incluye la libreria stdlib.h
